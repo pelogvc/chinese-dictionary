87397ae6c8b878720b05407ee7ce28396ea4a0e8
aa <pelogvc@gmail.com>
2019-10-18 02:13:37 +0900

    notosans font 적용

--- management/src/App.tsx
+++ management/src/App.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { PageHeader } from 'antd';
-import './App.css';
-import Header from './components/Header/Header';
-import usePage from './hooks/usePage';
-import Wordbook from './components/Wordbook/Wordbook';
-import TemplateMain from './components/Template/TemplateMain';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import TemplateMain from "./components/Template/TemplateMain";
+import Wordbook from "./components/Wordbook/Wordbook";
+import usePage from "./hooks/usePage";
 
 const App: React.FC = () => {
-
   const page = usePage();
 
-  if ( !page.id ) return <></>;
+  if (!page.id) {
+    return <></>;
+  }
 
   return (
-    <>
+    <div className="App">
       <Header />
       <div className="App-main">
-        { page.id === 'wordbook' && 
-          <TemplateMain title="단어장" >
+        {page.id === "wordbook" && (
+          <TemplateMain title="단어장">
             <Wordbook />
           </TemplateMain>
-        }
+        )}
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
